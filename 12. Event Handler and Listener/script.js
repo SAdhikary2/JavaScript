3252d0b8c3e1978caf252412af6725142c856255
Sukalyan <sadhikary0055@gmail.com>
2023-07-07 09:58:53 +0530

    commit

--- 12. Event Handler and Listener/script.js
+++ 12. Event Handler and Listener/script.js
@@ -30,18 +30,59 @@ function KeydownEvent(){
 // we can add here event object and get all the information about the event
 
 
-const box1=document.getElementById('box-1');
-box1.addEventListener('click',() => {
-    console.log('click by the event');
-});
+// const box1=document.getElementById('box-1');
+// box1.addEventListener('click',() => {
+//     console.log('click by the event');
+// });
 
 // adding another same click event
-box1.addEventListener('click',() => {
-    console.log('click by the event 2');
-});
+// box1.addEventListener('click',() => {
+//     console.log('click by the event 2');
+// });
 
 //adding the event object
-box1.addEventListener('click',(e) => {
-    console.log('event object',e);
+// box1.addEventListener('click',(e) => {
+//     console.log('event object',e);
+// });
+
+// box1.addEventListener('mousemove',(e) => {
+//     console.log('event object',e.clientX,e.clientY);
+// });
+
+ const nameInput=document.getElementById('nameinput');
+ nameInput.addEventListener("keypress",(e)=>{
+    console.log('key',e.key);
+ })
+
+ nameInput.addEventListener("focus",(e)=>{
+    console.log('focus',e);
+ })
+
+ //BUBLING AND CAPTURING
+
+ //BUBLING
+//inner element first and then the outer
+
+//  const box1=document.getElementById('box-1');
+// box1.addEventListener('click',() => {
+//     console.log('CLICKED ON THE BOX');
+// });
+
+// const container=document.getElementById('container');
+// box1.addEventListener('click',() => {
+//     console.log('CLICKED ON THE CONTAINER');
+// });
+
+//CAPTURE
+//outer element first and then the inner
+const box1=document.getElementById('box-1');
+box1.addEventListener('click',() => {
+    console.log('CLICKED ON THE BOX');
 });
+
+const container=document.getElementById('container');
+box1.addEventListener('click',() => {
+    console.log('CLICKED ON THE CONTAINER');
+},true);
+
 
